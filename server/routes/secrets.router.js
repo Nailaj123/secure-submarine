af9d284a44c2383b66ed87e7710b2b95ae8cf196
Your Name <nailaj123@gmail.com>
2020-06-25 00:41:30 -0500

    intal commit

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('req.user:', req.user);
+    const queryText = `SELECT * FROM "secret" WHERE "secrecy_level" < $1;`;
+    if (req.isAuthenticated()) {
+        pool.query(queryText, [req.user.clearance_level])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+module.exports = router;
